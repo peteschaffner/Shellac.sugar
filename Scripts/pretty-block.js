c3cd2d6fd34ccafb91bbbd5b2a6a5090ace73177
Pete Schaffner <pjschaffner@gmail.com>
2012-04-09 16:29:19 -0400

    Adding "Pretty block" text actions and bringing over @ianbeck's no-dupe.js script

---
+++ Scripts/pretty-block.js
@@ -0,0 +1,18 @@
+/**
+ * This action helps automatically format block statements (much like what is done in TextMate)
+ */
+ 
+action.canPerformWithContext = function (context, outError) {
+	// Grab the current cursor range
+	var range = context.selectedRanges[0];
+	// Find the character to the left of our cursor
+	var prevChar = context.substringWithRange(new Range(range.location - 1, 1));
+	
+	return prevChar === action.setup.character;
+};
+
+action.performWithContext = function (context, outError) {
+	var snippet = new CETextSnippet('\n\t$1\n');
+	
+	return context.insertTextSnippet(snippet);
+};
