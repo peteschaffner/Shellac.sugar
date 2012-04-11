1b0e538cbe48a7a702adf9d0f63559597327fb6c
Pete Schaffner <pjschaffner@gmail.com>
2012-04-11 10:46:34 -0400

    Adding separate action to cycle in reverse through color formats and doing other cleanup.

--- Scripts/convert_color.js
+++ Scripts/convert_color.js
@@ -3,22 +3,59 @@
  * Uses Russ Porosky's color2color function: http://indyarmy.com/
  */
  
- var color2color = loadLibrary('color2color');
+ var color = loadLibrary('color2color');
  
  action.canPerformWithContext = function(context, outError) {
 	var range = context.selectedRanges[0],
-		selection = context.substringWithRange(range));
+		selection = context.substringWithRange(range);
 	
-	console.log(selection);	
-	
-	if (range.length > 0) {
+	// Must have a selection and a setup direction
+	if (range.length > 0 && !!action.setup.direction) {
 		// If selection is a color type we understand return true
-		return (/^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/.test(selection) || (/^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d+(?:\.\d+)?|\.\d+)\s*\)/.test(selection) || (/^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/.test(selection) || (/^hsla\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%,\s*(\d+(?:\.\d+)?|\.\d+)\s*\)/.test(selection) || (/^([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/.test(selection) || (/^([0-9a-fA-F]{1})([0-9a-fA-F]{1})([0-9a-fA-F]{1})$/.test(selection);
+		return (/^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/.test(selection)) || (/^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d+(?:\.\d+)?|\.\d+)\s*\)/.test(selection)) || (/^hsl\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%\)$/.test(selection)) || (/^hsla\((\d{1,3}),\s*(\d{1,3})%,\s*(\d{1,3})%,\s*(\d+(?:\.\d+)?|\.\d+)\s*\)/.test(selection)) || (/^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/.test(selection)) || (/^#([0-9a-fA-F]{1})([0-9a-fA-F]{1})([0-9a-fA-F]{1})$/.test(selection));
+	} else {
+		return false;
 	}
 };
 
 action.performWithContext = function(context, outError) {
 	var range = context.selectedRanges[0],
-		selection = context.substringWithRange(range));
-	console.log(selection);
+		selection = context.substringWithRange(range), 
+		colorFormats = ['hex', 'rgb', 'rgba', 'hsl', 'hsla'],
+		curFormat,
+		nextFormat;
+		
+		// Capture our current format
+		if (/^#/.test(selection)) {
+			curFormat = 'hex';
+		} else if (/^rgb\(/.test(selection)) {
+			curFormat = 'rgb'
+		} else if (/^rgba/.test(selection)) {
+			curFormat = 'rgba'
+		} else if (/^hsl\(/.test(selection)) {
+			curFormat = 'hsl'
+		} else {
+			curFormat = 'hsla'
+		}
+		
+		// Cycle forward
+		if (action.setup.direction === 'next') {
+			if (colorFormats.indexOf(curFormat) + 1 === colorFormats.length) {
+				nextFormat = colorFormats[0]
+			} else {
+				nextFormat = colorFormats[colorFormats.indexOf(curFormat) + 1];
+			}
+		} else { // Cycle backward
+			if (colorFormats.indexOf(curFormat) === 0) {
+				nextFormat = colorFormats[colorFormats.length - 1]
+			} else {
+				nextFormat = colorFormats[colorFormats.indexOf(curFormat) - 1];
+			}
+		}
+		
+		var recipe = new CETextRecipe();
+		recipe.replaceRange(range, color.color2color(selection, nextFormat));
+		
+		// Apply the changes!
+		return context.applyTextRecipe(recipe);
 }
